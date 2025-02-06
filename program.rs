11a1855903283ee6c882e231e45358c200a737f5
Mnhthompson <mnhthompson@gmail.com>
2025-02-06 12:07:42 -0700

    version 1: to small currently

--- program.rs
+++ program.rs
@@ -1,11 +1,13 @@
 use std::io;
 
-
 fn main() {
 
+    loop {
     //Define a variable 'number' and assign a value to it
+    print!("Type a whole number between 1 - 32: ");
 
-    println!("Type a number : ");
+    // Ensure text appears before input
+    io::Write::flush(&mut io::stdout()).expect("Failed to flush stdout"); 
 
     let mut line = String::new();
 
@@ -13,10 +15,29 @@ fn main() {
         .read_line(&mut line)
         .expect("Failed to read line");
 
-    let number: u64 = line
-        .trim()
-        .parse()
-        .expect("Wanted a number");
+        let number: u128 = match line.trim().parse() {
+            Ok(n) => n,
+         // error protection
+            Err(_) => {
+                println!("⚠️ Invalid input! ⚠️ Please enter a valid number.");
+                continue; // Restart the loop
+            }
+        };
+
+        let numberexit = number.to_string();
+
+        // error protection
+        if number > 32 {
+            // Restart the loop
+            println!("⚠️Number too large! ⚠️ Please enter a whole number between 1 and 32.");
+            continue;
+        }
+
+        // quit out the app
+        if numberexit.to_lowercase() == "0" {
+            println!("Exiting");
+            break;
+        }
 
     // Call the 'factorial' function with 'number' as an argument
     // Store the result in a variable named 'factorial_result'
@@ -24,10 +45,13 @@ fn main() {
 
     // Print the result
     println!("Factorial of {} is: {}", number, factorial_result);
-}
 
-// Define a function named 'factorial' that takes a parameter 'n' of type u64 and returns a u64
-fn factorial(n: u64) -> u64 {
+    println!("Type 0 to Exit ");
+
+}}
+
+// Define a function named 'factorial' that takes a parameter 'n' of type u128 and returns a u128
+fn factorial(n: u128) -> u128 {
     // Initialize the result variable with 1
     let mut result = 1;
 
